b92a0fa27ae71f30702c363acf1faa5f8e2f015e
Minhazul Abedin <73342997+min23a@users.noreply.github.com>
2021-02-05 15:23:32 +0600

    Update main.js
    update

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function getCityName() {
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=61aed59fcb99d7ea22aa848f4b31a033')
         .then(res => res.json())
         .then(data =>{
-            document.getElementById("area_temp").innerText = "" +parseInt(data.main.temp -273) || "unknown location";
-            document.getElementById("area_status").innerText = data.weather[0].description || "unknown location";
+            document.getElementById("area_temp").innerText = "" +parseInt(data.main.temp -273);
+            document.getElementById("area_status").innerText = data.weather[0].description;
             document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-            document.getElementById("area_name").innerText = data.name || "unknown location";
+            document.getElementById("area_name").innerText = data.name;
         }
         );
 }
